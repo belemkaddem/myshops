06d7ea56231c3dde2e0be0d1e9260938ef9980af
belemkaddem <33959770+belemkaddem@users.noreply.github.com>
2017-12-04 13:47:00 +0000

    Add files via upload

---
+++ src/main/java/org/demo/myshops/myshops/MyshopsApplication.java
@@ -0,0 +1,12 @@
+package org.demo.myshops.myshops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyshopsApplication.class, args);
+	}
+}
